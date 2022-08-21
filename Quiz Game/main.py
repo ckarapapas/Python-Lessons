484429a9985c6de907dcf6612611d0d7899e0b5a
ckarapapas <ckarapapas@gmail.com>
2022-08-22 02:51:47 +0300

    Complete Quiz Game

--- Quiz Game/main.py
+++ Quiz Game/main.py
@@ -1,10 +1,16 @@
-import data
+import data, quiz_brain
 from question_model import Question
 
 question_bank = []
 
-for i in range(0,len(data.question_data)):
+for i in range(0, len(data.question_data)):
+    #print(i)
     new_q = Question(data.question_data[i]["text"], data.question_data[i]["answer"])
     question_bank.append(new_q)
-print(question_bank)
+quizb = quiz_brain.QuizBrain(question_bank)
 
+while quizb.still_has_questions():
+    quizb.next_question()
+
+print("You completed the quiz!")
+print(f"Final score is: {quizb.score/len(question_bank)}")
--- Quiz Game/question_model.py
+++ Quiz Game/question_model.py
@@ -7,3 +7,4 @@ class Question:
     def __init__(self, text, answer):
         self.text = text
         self.answer = answer
+
